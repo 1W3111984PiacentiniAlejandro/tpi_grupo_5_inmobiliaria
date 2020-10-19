da6e03c90edd8f4d462113f6b97442d2927b9264
1W3111984PiacentiniAlejandro <69598780+1W3111984PiacentiniAlejandro@users.noreply.github.com>
2020-10-19 18:07:12 -0300

    puse mensaje
    messa

--- TPI_Grupo_5_Inmobiliaria/Form1.cs
+++ TPI_Grupo_5_Inmobiliaria/Form1.cs
@@ -15,7 +15,7 @@ namespace TPI_Grupo_5_Inmobiliaria
         public Form1()
         {
             InitializeComponent();
-            // esto es para verificar
+            MessageBox.Show(" esto es para verificar");
         }
     }
 }
